d771da5efbecf6c7f1714ffc741689f8ff441880
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-10-17 14:02:12 +0300

    model-mongoose

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -4,7 +4,6 @@ const { RequestError } = require("../../helpers");
 
 const getBuId = async (req, res) => {
   const { contactId } = req.params;
-  // const result = await Contact.findOne({_id: contactId});
   const result = await Contact.findById(contactId);
   if (!result) {
     throw RequestError(404, `Not found`);
