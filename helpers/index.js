a698042c2767980482c3e592eea5b46429697867
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-11-13 23:37:26 +0200

    верифікація-email

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,13 @@
 const RequestError = require("./RequestError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleSaveErrors = require("./handleSaveErrors");
+const sendVerifyEmail = require('./sendVerifyEmail');
+const createVerifyMail = require('./createVerifyMail');
 
 module.exports = {
     RequestError,
     ctrlWrapper,
     handleSaveErrors,
+    sendVerifyEmail,
+    createVerifyMail,
 }
